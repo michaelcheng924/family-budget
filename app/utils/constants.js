117ab0a668bc3287e8765f7e5d3fcf66c348c6be
Michael Cheng <cheng.c.mike@gmail.com>
2018-06-15 16:47:46 -0500

    fix indexes

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,196 +1,201 @@
 export const dateIndex = 0;
 export const descriptionIndex = 1;
-export const incomeIndex = 2;
-export const totalIndex = 3;
-export const givingGFBCExpensesIndex = 4;
-export const givingGFBCTotalIndex = 5;
-export const givingOtherExpensesIndex = 6;
-export const givingOtherTotalIndex = 7;
-export const savingsExpensesIndex = 8;
-export const savingsTotalIndex = 9;
-export const housingExpensesIndex = 10;
-export const housingTotalIndex = 11;
-export const utilitiesExpensesIndex = 12;
-export const utilitiesTotalIndex = 13;
-export const generalHousingExpensesIndex = 14;
-export const generalHousingTotalIndex = 15;
-export const gasExpensesIndex = 16;
-export const gasTotalIndex = 17;
-export const carMaintenanceExpensesIndex = 18;
-export const carMaintenanceTotalIndex = 19;
-export const carInsuranceExpensesIndex = 20;
-export const carInsuranceTotalIndex = 21;
-export const groceryExpensesIndex = 22;
-export const groceryTotalIndex = 23;
-export const clothingMichaelExpensesIndex = 24;
-export const clothingMichaelTotalIndex = 25;
-export const clothingAnnaExpensesIndex = 26;
-export const clothingAnnaTotalIndex = 27;
-export const giftsExpensesIndex = 28;
-export const giftsTotalIndex = 29;
-export const educationExpensesIndex = 30;
-export const educationTotalIndex = 31;
-export const booksExpensesIndex = 32;
-export const booksTotalIndex = 33;
-export const entertainmentExpensesIndex = 34;
-export const entertainmentTotalIndex = 35;
-export const funMichaelExpensesIndex = 36;
-export const funMichaelTotalIndex = 37;
-export const funAnnaExpensesIndex = 38;
-export const funAnnaTotalIndex = 39;
-export const travelExpensesIndex = 40;
-export const travelTotalIndex = 41;
-export const healthInsuranceExpensesIndex = 42;
-export const healthInsuranceTotalIndex = 43;
-export const healthExpensesIndex = 44;
-export const healthTotalIndex = 45;
-export const miscExpensesIndex = 46;
-export const miscTotalIndex = 47;
-export const facebookAdsExpensesIndex = 48;
-export const facebookAdsTotalIndex = 49;
-export const businessExpensesIndex = 50;
-export const businessTotalIndex = 51;
+export const fromSavingsIndex = 2;
+export const incomeIndex = 3;
+export const totalIndex = 4;
+export const givingGFBCExpensesIndex = 5;
+export const givingGFBCTotalIndex = 6;
+export const givingOtherExpensesIndex = 7;
+export const givingOtherTotalIndex = 8;
+export const savingsExpensesIndex = 9;
+export const savingsTotalIndex = 10;
+export const housingExpensesIndex = 11;
+export const housingTotalIndex = 12;
+export const utilitiesExpensesIndex = 13;
+export const utilitiesTotalIndex = 14;
+export const generalHousingExpensesIndex = 15;
+export const generalHousingTotalIndex = 16;
+export const gasExpensesIndex = 17;
+export const gasTotalIndex = 18;
+export const carMaintenanceExpensesIndex = 19;
+export const carMaintenanceTotalIndex = 20;
+export const carInsuranceExpensesIndex = 21;
+export const carInsuranceTotalIndex = 22;
+export const groceryExpensesIndex = 23;
+export const groceryTotalIndex = 24;
+export const clothingMichaelExpensesIndex = 25;
+export const clothingMichaelTotalIndex = 26;
+export const clothingAnnaExpensesIndex = 27;
+export const clothingAnnaTotalIndex = 28;
+export const giftsExpensesIndex = 29;
+export const giftsTotalIndex = 30;
+export const educationExpensesIndex = 31;
+export const educationTotalIndex = 32;
+export const booksExpensesIndex = 33;
+export const booksTotalIndex = 34;
+export const entertainmentExpensesIndex = 35;
+export const entertainmentTotalIndex = 36;
+export const funMichaelExpensesIndex = 37;
+export const funMichaelTotalIndex = 38;
+export const funAnnaExpensesIndex = 39;
+export const funAnnaTotalIndex = 40;
+export const travelExpensesIndex = 41;
+export const travelTotalIndex = 42;
+export const healthInsuranceExpensesIndex = 43;
+export const healthInsuranceTotalIndex = 44;
+export const healthExpensesIndex = 45;
+export const healthTotalIndex = 46;
+export const miscExpensesIndex = 47;
+export const miscTotalIndex = 48;
+export const facebookAdsExpensesIndex = 49;
+export const facebookAdsTotalIndex = 50;
+export const businessExpensesIndex = 51;
+export const businessTotalIndex = 52;
 
 export const dateMapping = {
-    0: '01',
-    1: '02',
-    2: '03',
-    3: '04',
-    4: '05',
-    5: '06',
-    6: '07',
-    7: '08',
-    8: '09',
-    9: '10',
-    10: '11',
-    11: '12',
-    116: '2016',
-    117: '2017',
-    118: '2018',
-    119: '2019'
+  0: "01",
+  1: "02",
+  2: "03",
+  3: "04",
+  4: "05",
+  5: "06",
+  6: "07",
+  7: "08",
+  8: "09",
+  9: "10",
+  10: "11",
+  11: "12",
+  116: "2016",
+  117: "2017",
+  118: "2018",
+  119: "2019"
 };
 
 export const categoriesMapping = [
-    {
-        name: 'Income',
-        index: incomeIndex
-    },
-    {
-        name: 'Giving, GfBC',
-        index: givingGFBCExpensesIndex
-    },
-    {
-        name: 'Giving, other',
-        index: givingOtherExpensesIndex,
-        budget: 350
-    },
-    {
-        name: 'Savings',
-        index: savingsExpensesIndex
-    },
-    {
-        name: 'Housing',
-        index: housingExpensesIndex,
-        budget: 1200
-    },
-    {
-        name: 'Utilities',
-        index: utilitiesExpensesIndex,
-        budget: 250
-    },
-    {
-        name: 'General Housing',
-        index: generalHousingExpensesIndex,
-        budget: 50
-    },
-    {
-        name: 'Gas',
-        index: gasExpensesIndex,
-        budget: 200
-    },
-    {
-        name: 'Car Maintenance',
-        index: carMaintenanceExpensesIndex,
-        budget: 200
-    },
-    {
-        name: 'Car Insurance',
-        index: carInsuranceExpensesIndex,
-        budget: 130
-    },
-    {
-        name: 'Grocery',
-        index: groceryExpensesIndex,
-        budget: 400
-    },
-    {
-        name: 'Clothing Michael',
-        index: clothingMichaelExpensesIndex,
-        budget: 25
-    },
-    {
-        name: 'Clothing Anna',
-        index: clothingAnnaExpensesIndex,
-        budget: 25
-    },
-    {
-        name: 'Gifts',
-        index: giftsExpensesIndex,
-        budget: 25
-    },
-    {
-        name: 'Education',
-        index: educationExpensesIndex,
-        budget: 40
-    },
-    {
-        name: 'Books',
-        index: booksExpensesIndex,
-        budget: 25
-    },
-    {
-        name: 'Entertainment',
-        index: entertainmentExpensesIndex,
-        budget: 50
-    },
-    {
-        name: 'Fun Michael',
-        index: funMichaelExpensesIndex,
-        budget: 25
-    },
-    {
-        name: 'Fun Anna',
-        index: funAnnaExpensesIndex,
-        budget: 25
-    },
-    {
-        name: 'Travel',
-        index: travelExpensesIndex,
-        budget: 170
-    },
-    {
-        name: 'Health Insurance',
-        index: healthInsuranceExpensesIndex,
-        budget: 220
-    },
-    {
-        name: 'Health',
-        index: healthExpensesIndex,
-        budget: 50
-    },
-    {
-        name: 'Miscellaneous',
-        index: miscExpensesIndex,
-        budget: 100
-    },
-    {
-        name: 'Facebook Ads',
-        index: facebookAdsExpensesIndex,
-        budget: 450
-    },
-    {
-        name: 'Business Expenses',
-        index: businessExpensesIndex,
-        budget: 25
-    }
+  {
+    name: "From Savings",
+    index: fromSavingsIndex
+  },
+  {
+    name: "Income",
+    index: incomeIndex
+  },
+  {
+    name: "Giving, GfBC",
+    index: givingGFBCExpensesIndex
+  },
+  {
+    name: "Giving, other",
+    index: givingOtherExpensesIndex,
+    budget: 350
+  },
+  {
+    name: "Savings",
+    index: savingsExpensesIndex
+  },
+  {
+    name: "Housing",
+    index: housingExpensesIndex,
+    budget: 1200
+  },
+  {
+    name: "Utilities",
+    index: utilitiesExpensesIndex,
+    budget: 250
+  },
+  {
+    name: "General Housing",
+    index: generalHousingExpensesIndex,
+    budget: 50
+  },
+  {
+    name: "Gas",
+    index: gasExpensesIndex,
+    budget: 200
+  },
+  {
+    name: "Car Maintenance",
+    index: carMaintenanceExpensesIndex,
+    budget: 200
+  },
+  {
+    name: "Car Insurance",
+    index: carInsuranceExpensesIndex,
+    budget: 130
+  },
+  {
+    name: "Grocery",
+    index: groceryExpensesIndex,
+    budget: 400
+  },
+  {
+    name: "Clothing Michael",
+    index: clothingMichaelExpensesIndex,
+    budget: 25
+  },
+  {
+    name: "Clothing Anna",
+    index: clothingAnnaExpensesIndex,
+    budget: 25
+  },
+  {
+    name: "Gifts",
+    index: giftsExpensesIndex,
+    budget: 25
+  },
+  {
+    name: "Education",
+    index: educationExpensesIndex,
+    budget: 40
+  },
+  {
+    name: "Books",
+    index: booksExpensesIndex,
+    budget: 25
+  },
+  {
+    name: "Entertainment",
+    index: entertainmentExpensesIndex,
+    budget: 50
+  },
+  {
+    name: "Fun Michael",
+    index: funMichaelExpensesIndex,
+    budget: 25
+  },
+  {
+    name: "Fun Anna",
+    index: funAnnaExpensesIndex,
+    budget: 25
+  },
+  {
+    name: "Travel",
+    index: travelExpensesIndex,
+    budget: 170
+  },
+  {
+    name: "Health Insurance",
+    index: healthInsuranceExpensesIndex,
+    budget: 220
+  },
+  {
+    name: "Health",
+    index: healthExpensesIndex,
+    budget: 50
+  },
+  {
+    name: "Miscellaneous",
+    index: miscExpensesIndex,
+    budget: 100
+  },
+  {
+    name: "Facebook Ads",
+    index: facebookAdsExpensesIndex,
+    budget: 450
+  },
+  {
+    name: "Business Expenses",
+    index: businessExpensesIndex,
+    budget: 25
+  }
 ];
